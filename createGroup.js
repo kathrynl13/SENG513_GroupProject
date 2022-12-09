54a3aae9343ce52fc01b987a5926e5d4b2a21744
Hajer35 <hajer352009@gmail.com>
2022-12-09 16:52:22 -0700

    Create group page updated : - Rules are dynamic - prevent group creation on empty inputs

--- createGroup.js
+++ createGroup.js
@@ -1,43 +1,110 @@
-const socket = io.connect('http://localhost:5000')
+const socket = io.connect("http://localhost:5000");
 
-const updateButton = document.getElementById('updateButton'),
-  cancelButton = document.getElementById('cancelButton'),
-  addRuleButton = document.getElementById('addRuleButton'),
-  name = document.getElementById('Name')
+var cancelButton = document.getElementById('cancelButton'),
+    addRuleButton = document.getElementById('addRuleButton');
 
-updateButton.addEventListener('click', updateClicked)
 
-cancelButton.addEventListener('click', cancelClicked)
 
-addRuleButton.addEventListener('click', addRuleClicked)
+updateButton.addEventListener('click', updateClicked);
+
+cancelButton.addEventListener('click', cancelClicked);
+
+addRuleButton.addEventListener('click', addRuleClicked);
+var ruleId = 0;
+
+
+function rulesFilled(array) {
+    var filled = true;
+    for (let i = 0; i < array.length; i++) {
+        let buyer = array[i].children[0].value;
+        let boughtFor = array[i].children[2].value;
+        console.log("buyer", buyer.length, "boughtFor", boughtFor.length, " ", (buyer.length == 0 || boughtFor.length == 0))
+        if ((buyer.length == 0 && boughtFor.length > 0) || (buyer.length > 0 && boughtFor.length == 0)) {
+            console.log("buyer", buyer, "boughtFor", boughtFor)
+            filled = false;
+            break;
+        }
+
+    }
+    console.log("filled", filled)
+    return filled;
+
+}
+
+function groupDetailsFilled(name, dueDate, priceLimit) {
+
+    var filled = name.length > 0 && dueDate.length > 0 && priceLimit.length > 0;
+    console.log('name ', name, ' dueDate ', dueDate, ' priceLimit', priceLimit, ' == ', filled);
+    return filled;
+}
 
 function updateClicked(e) {
-  if (PriceLimit.value == '' || Name.value == '' || DueDate.value == '') {
-    alert('Field cannot be empty')
-    return
-  }
+    // var GroupName = document.getElementById('GroupName'),
+    //     PriceLimit = document.getElementById('PriceLimit'),
+    //     DueDate = document.getElementById('DueDate');
 
-  socket.emit('GroupInfoInputted', Name.value, PriceLimit.value, DueDate.value)
-  console.log(Name + ' ' + PriceLimit + ' ' + DueDate)
+    const array = document.getElementById("rules").children;
+    //GroupName.value, DueDate.value, PriceLimit.value);
+
+    if (groupDetailsFilled(GroupName.value, DueDate.value, PriceLimit.value) && rulesFilled(array)) {
+        socket.emit("GroupInfoInputted", GroupName.value[0], PriceLimit.value, DueDate.value);
+        console.log(GroupName.value + " " + PriceLimit.value + " " + DueDate.value);
+
+
+        for (let i = 0; i < array.length; i++) {
+            let buyer = array[i].children[0].value;
+            let boughtFor = array[i].children[2].value;
+            console.log("buyer", buyer, "boughtFor", boughtFor)
+        }
+
+        socket.emit("rulesInputted", array);
+
+
+        window.location.href = "SantaGroups.html";
+    } else {
+        console.log('here');
+        alert("Field cannot be empty");
+        return;
+
+
+    }
+
+
 }
 
 function cancelClicked(e) {
-  alert('Discard group?')
-  window.location.href = 'SantaGroups.html'
+    alert("Discard group?");
+    window.location.href = "SantaGroups.html";
 }
 
 function addRuleClicked(e) {
-  if (rulePerson1.value == '' || rulePerson2.value == '') {
-    alert('Field cannot be empty')
-  }
 
-  socket.emit('rulesInputted', rulePerson1.value)
-  socket.emit(rulePerson2.value)
-  console.log(rulePerson1 + ' ' + rulePerson2)
+    ruleId++;
+
+    var form = document.getElementById("rules");
+    var rule1 = document.createElement("div");
+    rule1.style = "block";
+    form.appendChild(rule1);
+
+    var input1 = document.createElement("input");
+    input1.type = "text";
+    input1.className = "rulePerson1";
+    input1.id = "Person" + ruleId;
+    input1.name = "rules";
+    rule1.appendChild(input1);
+
+    var label = document.createElement("label");
+    label.className = "ruleLabel";
+    label.id = "rule1Label";
+    label.style.color = "#F92545";
+    label.textContent = "Cannot buy gift for";
+    rule1.appendChild(label);
+    ruleId++;
+    var input2 = document.createElement("input");
+    input2.type = "text";
+    input2.className = "rulePerson2";
+    input2.id = "Person" + ruleId;
+    input2.name = "rules";
+    rule1.appendChild(input2);
+
 }
-
-socket.on('groupCreated', (code) => {
-  console.log('creatGROup.js code', code)
-  alert('Your group has been successfully created. Your join code is: ' + code)
-  window.location.href = 'SantaGroups.html'
-})
