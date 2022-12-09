f6f1e8e090edbfc389fc8ba0a6e040935afd066c
Hajer35 <hajer352009@gmail.com>
2022-12-09 11:14:54 -0700

    Create with cap is redundant

--- createGroup.js
+++ createGroup.js
@@ -1,43 +1,43 @@
-const socket = io.connect("http://localhost:5000");
+const socket = io.connect('http://localhost:5000')
 
 const updateButton = document.getElementById('updateButton'),
-    cancelButton = document.getElementById('cancelButton'),
-    addRuleButton = document.getElementById('addRuleButton'),
-    name = document.getElementById('Name');
-    
-updateButton.addEventListener('click', updateClicked);
+  cancelButton = document.getElementById('cancelButton'),
+  addRuleButton = document.getElementById('addRuleButton'),
+  name = document.getElementById('Name')
 
-cancelButton.addEventListener('click', cancelClicked);
+updateButton.addEventListener('click', updateClicked)
 
-addRuleButton.addEventListener('click', addRuleClicked);
+cancelButton.addEventListener('click', cancelClicked)
 
-function updateClicked(e){
-    if (PriceLimit.value == "" || Name.value == "" || DueDate.value == ""){
-        alert("Field cannot be empty");
-        return;
-    }
+addRuleButton.addEventListener('click', addRuleClicked)
 
-    socket.emit("GroupInfoInputted", Name.value, PriceLimit.value, DueDate.value);
-    console.log(Name + " " + PriceLimit + " " + DueDate);
+function updateClicked(e) {
+  if (PriceLimit.value == '' || Name.value == '' || DueDate.value == '') {
+    alert('Field cannot be empty')
+    return
+  }
+
+  socket.emit('GroupInfoInputted', Name.value, PriceLimit.value, DueDate.value)
+  console.log(Name + ' ' + PriceLimit + ' ' + DueDate)
 }
 
-function cancelClicked(e){
-    alert("Discard group?");
-    window.location.href="SantaGroups.html";
+function cancelClicked(e) {
+  alert('Discard group?')
+  window.location.href = 'SantaGroups.html'
 }
 
-function addRuleClicked(e){
-    if (rulePerson1.value == "" || rulePerson2.value == ""){
-        alert("Field cannot be empty");
-    }
+function addRuleClicked(e) {
+  if (rulePerson1.value == '' || rulePerson2.value == '') {
+    alert('Field cannot be empty')
+  }
 
-    socket.emit("rulesInputted", rulePerson1.value);
-    socket.emit(rulePerson2.value);
-    console.log(rulePerson1 + " " + rulePerson2);
+  socket.emit('rulesInputted', rulePerson1.value)
+  socket.emit(rulePerson2.value)
+  console.log(rulePerson1 + ' ' + rulePerson2)
 }
 
-socket.on("groupCreated", (code) => {
-    console.log("creatGROup.js code", code);
-    alert("Your group has been successfully created. Your join code is: "+code);
-    window.location.href="SantaGroups.html";
+socket.on('groupCreated', (code) => {
+  console.log('creatGROup.js code', code)
+  alert('Your group has been successfully created. Your join code is: ' + code)
+  window.location.href = 'SantaGroups.html'
 })
