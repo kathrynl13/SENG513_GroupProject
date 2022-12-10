05fbd7fe9ed53bb03bb4440ad26bbf464f734b7e
kathrynl13 <47461692+kathrynl13@users.noreply.github.com>
2022-12-09 23:35:42 -0700

    Add files via upload

--- createGroup.js
+++ createGroup.js
@@ -1,108 +1,60 @@
 const socket = io.connect("http://localhost:5000");
 
-var cancelButton = document.getElementById('cancelButton'), 
-    updateButton = document.getElementById('updateButton');
-   // addRuleButton = document.getElementById('addRuleButton');
+let memberID = getCookie('memberid')
 
 
+const updateButton = document.getElementById('updateButton')
+const cancelButton = document.getElementById('cancelButton')
+const aName = document.getElementById('Name')
+const aDueDate = document.getElementById('DueDate')
+const aPriceLimit = document.getElementById('PriceLimit')
 
 updateButton.addEventListener('click', updateClicked);
 
 cancelButton.addEventListener('click', cancelClicked);
 
 //addRuleButton.addEventListener('click', addRuleClicked);
-var ruleId = 0;
 
-
-function rulesFilled(array) {
-    var filled = true;
-    for (let i = 0; i < array.length; i++) {
-        let buyer = array[i].children[0].value;
-        let boughtFor = array[i].children[2].value;
-        console.log("buyer", buyer.length, "boughtFor", boughtFor.length, " ", (buyer.length == 0 || boughtFor.length == 0))
-        if ((buyer.length == 0 && boughtFor.length > 0) || (buyer.length > 0 && boughtFor.length == 0)) {
-            console.log("buyer", buyer, "boughtFor", boughtFor)
-            filled = false;
-            break;
-        }
-    }
-    console.log("filled", filled)
-    return filled;
-}
-
-function groupDetailsFilled(name, dueDate, priceLimit) {
-
-    var filled = name.length > 0 && dueDate.length > 0 && priceLimit.length > 0;
-    console.log('name ', name, ' dueDate ', dueDate, ' priceLimit', priceLimit, ' == ', filled);
-    return filled;
-}
-
-function updateClicked(e) {
-    console.log("hehhre")
-    var GroupName = document.getElementById('GroupName'),
-    PriceLimit = document.getElementById('PriceLimit'),
-    DueDate = document.getElementById('DueDate');
-
-    //const array = document.getElementById("rules").children;
-    //GroupName.value, DueDate.value, PriceLimit.value);
-    console.log(GroupName.value + " " + PriceLimit.value + " " + DueDate.value);
-    if (groupDetailsFilled(GroupName.value, DueDate.value, PriceLimit.value)) {
-        socket.emit("GroupInfoInputted", GroupName.value[0], PriceLimit.value, DueDate.value);
-        console.log(GroupName.value + " " + PriceLimit.value + " " + DueDate.value);
-
-
-        /* for (let i = 0; i < array.length; i++) {
-            let buyer = array[i].children[0].value;
-            let boughtFor = array[i].children[2].value;
-            console.log("buyer", buyer, "boughtFor", boughtFor)
-        }
-
-        socket.emit("rulesInputted", array); */
-
-
-        window.location.href = "SantaGroups.html";
-    } else {
-        console.log('here');
+function updateClicked(e){
+    if ((aPriceLimit.value == "") || (aName.value == "") || (aDueDate.value == "")){
         alert("Field cannot be empty");
         return;
     }
 
-
+    socket.emit("GroupInfoInputted", Name.value, PriceLimit.value, DueDate.value, memberID);
+    console.log(Name + " " + PriceLimit + " " + DueDate);
 }
 
-function cancelClicked(e) {
+function cancelClicked(e){
     alert("Discard group?");
-    window.location.href = "SantaGroups.html";
+    window.location.href="SantaGroups.html";
 }
 
-function addRuleClicked(e) {
+/* function addRuleClicked(e){
+    if (rulePerson1.value == "" || rulePerson2.value == ""){
+        alert("Field cannot be empty");
+    }
 
-    ruleId++;
+    socket.emit("rulesInputted", rulePerson1.value);
+    socket.emit(rulePerson2.value);
+    console.log(rulePerson1 + " " + rulePerson2);
+} */
 
-    var form = document.getElementById("rules");
-    var rule1 = document.createElement("div");
-    rule1.style = "block";
-    form.appendChild(rule1);
+socket.on("groupCreated", (code) => {
+    console.log("creatGroup.js code", code);
+    alert("Your group has been successfully created. Your join code is: "+code);
+    window.location.href="SantaGroups.html";
+})
 
-    var input1 = document.createElement("input");
-    input1.type = "text";
-    input1.className = "rulePerson1";
-    input1.id = "Person" + ruleId;
-    input1.name = "rules";
-    rule1.appendChild(input1);
-
-    var label = document.createElement("label");
-    label.className = "ruleLabel";
-    label.id = "rule1Label";
-    label.style.color = "#F92545";
-    label.textContent = "Cannot buy gift for";
-    rule1.appendChild(label);
-    ruleId++;
-    var input2 = document.createElement("input");
-    input2.type = "text";
-    input2.className = "rulePerson2";
-    input2.id = "Person" + ruleId;
-    input2.name = "rules";
-    rule1.appendChild(input2);
-
+///////////////////////// https://stackoverflow.com/questions/2144386/how-to-delete-a-cookie/////////////////////////////
+function getCookie(name) {
+    var nameEQ = name + '='
+    var ca = document.cookie.split(';')
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i]
+        while (c.charAt(0) == ' ') c = c.substring(1, c.length)
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length)
+    }
+    return null
 }
+  /////////////////////////end of cited code//////////////////////////////////////
